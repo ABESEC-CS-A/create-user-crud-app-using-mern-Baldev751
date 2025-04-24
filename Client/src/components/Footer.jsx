ad81dba56dfd9498674cd46c84ff344e3f71d3da
Baldev751 <baldev.22b1531001@abes.ac.in>
2025-04-24 11:59:21 +0530

    1st commit

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h5>By Baldev Kumar designing of user app</h5>
+    </div>
+  )
+}
+
+export default Footer
